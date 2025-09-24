739b13484a67b8389f9ada6659608ce88948a531
Abraarcodes <mohammedabraar360@gmail.com>
2025-09-24 13:36:58 +0530

    Add user routes

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { getCurrentUser, loginUser, registerUser, updatePassword, updateProfile } from '../controllers/userController.js';
+import authMiddleware from '../middleware/auth.js'
+const userRouter = express.Router();
+
+
+//public links
+userRouter.post('/register',registerUser)
+userRouter.post('/login',loginUser)
+
+
+//private links (protected)
+userRouter.get('/me',authMiddleware,getCurrentUser);
+userRouter.put('/profile',authMiddleware,updateProfile);
+userRouter.put('/password',authMiddleware,updatePassword)
+
+
+export default userRouter;
